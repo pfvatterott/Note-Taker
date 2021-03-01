497696319475de74fee08e2f8ea7e73bd0a526b7
Paul <pfvatterott@gmail.com>
2021-03-01 12:00:59 -0700

    Cleaned up more unnecessary code

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+// Gets index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// Get notes.html
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// Get API
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 })
 
+// Save a Note
 app.post('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(err, data) {
         if (err) {
@@ -40,6 +44,7 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
+// Delete Notes
 app.delete('/api/notes/:note', (req, res) => {
     fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(err, data) {
         if (err) {
@@ -55,20 +60,14 @@ app.delete('/api/notes/:note', (req, res) => {
     })
 });
 
+// Edit Notes
 app.post('/api/notes/:index', (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(err, data) {
-        if (err) {
-            console.log(err)
-        };
-        let notes = JSON.parse(data);
-        let newNotes = (req.body)
-        console.log(newNotes);
-        const index = notes.findIndex(r => r.id === req.params.id)
-        fs.writeFile("./db/db.json", JSON.stringify(newNotes), function (err) {
-            if (err) throw (err)
-        });
-        res.redirect("/notes");
-    })
+    let newNotes = (req.body)
+    fs.writeFile("./db/db.json", JSON.stringify(newNotes), function (err) {
+        if (err) throw (err)
+    });
+    res.redirect("/notes");
+
 });
 
 // ===============================================
