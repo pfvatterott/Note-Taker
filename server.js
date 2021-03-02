cdb4da87587facf33b1922d36d1c0176de435410
Paul <pfvatterott@gmail.com>
2021-03-01 19:29:13 -0700

    Comments

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post('/api/notes/:index', (req, res) => {
     res.redirect("/notes");
 });
 
+// Wildcard backup
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
